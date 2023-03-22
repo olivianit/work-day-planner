8dde29ca3a6efb993db23a91c3a7c445954b9668
Aivi <olivianitzel@gmail.com>
2023-03-22 12:37:50 -0400

    localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -36,4 +36,26 @@ function updateBlocks() {
       $(this).removeClass("present")
       $(this).addClass("future");
     }
-    
+  });
+}
+
+updateBlocks();
+
+$('#hour-5 .description').val(localStorage.getItem('hour-5'));
+$('#hour-6 .description').val(localStorage.getItem('hour-6'));
+$('#hour-7 .description').val(localStorage.getItem('hour-7'));
+$('#hour-8 .description').val(localStorage.getItem('hour-8'));
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17'));
+$('#hour-18 .description').val(localStorage.getItem('hour-18'));
+$('#hour-19 .description').val(localStorage.getItem('hour-19'));
+
+// Code that will display the current date in the header of the page
+$('#currentDay').text(dayjs().format('ddd, MMM D, YYYY'));
